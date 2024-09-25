ccd8252f7b7f316d8b17611c67abd2c0ac1fbd68
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-09-25 15:07:39 -0300

    income-tax-service: create TaxRate domain entity

---
+++ income-tax-service/src/main/java/com/gabrielluciano/incometaxservice/domain/model/TaxRate.java
@@ -0,0 +1,38 @@
+package com.gabrielluciano.incometaxservice.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tax_rate")
+@SequenceGenerator(name = TaxRate.SEQUENCE_NAME, sequenceName = TaxRate.SEQUENCE_NAME)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaxRate implements Comparable<TaxRate> {
+
+    public static final String SEQUENCE_NAME = "sequence_tax_rate";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME)
+    private Long id;
+    @Column(nullable = false, unique = true, precision = 10, scale = 2)
+    private BigDecimal minimumSalaryThreshold;
+    @Column(nullable = false, unique = true, precision = 10, scale = 2)
+    private BigDecimal maximumSalaryThreshold;
+    @Column(nullable = false, precision = 4, scale = 2)
+    private BigDecimal rate;
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal deduction;
+
+    @Override
+    public int compareTo(TaxRate o) {
+        return minimumSalaryThreshold.compareTo(o.minimumSalaryThreshold);
+    }
+}
