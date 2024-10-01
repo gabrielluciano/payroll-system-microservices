2778f8c4e1b06de4fcf8d6d5965b65c39d110618
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-09-30 23:01:43 -0300

    work-attendance-publish-service: refactor tests

--- work-attendance-publish-service/src/test/java/com/gabrielluciano/workattendancepublishservice/application/resource/WorkAttendanceResourceTest.java
+++ work-attendance-publish-service/src/test/java/com/gabrielluciano/workattendancepublishservice/application/resource/WorkAttendanceResourceTest.java
@@ -1,21 +1,30 @@
 package com.gabrielluciano.workattendancepublishservice.application.resource;
 
 import com.gabrielluciano.workattendancepublishservice.domain.dto.CreateWorkAttendanceRequest;
+import com.gabrielluciano.workattendancepublishservice.domain.model.WorkAttendanceRecord;
 import com.gabrielluciano.workattendancepublishservice.util.JsonUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.concurrent.CompletableFuture;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(properties = {"eureka.client.enabled=false"})
 @AutoConfigureMockMvc
 class WorkAttendanceResourceTest {
 
@@ -25,161 +34,133 @@ class WorkAttendanceResourceTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private KafkaTemplate<String, WorkAttendanceRecord> kafkaTemplate;
+
     @Test
-    @DisplayName("Should save work attendance record")
-    void shouldSaveWorkAttendanceRecord() throws Exception {
-        int year = 2024;
-        int month = 10;
-        var request = new CreateWorkAttendanceRequest(VALID_CPF, year, month, 180, 180);
+    @DisplayName("Should publish work attendance record to kafka")
+    void shouldPublishSaveWorkAttendanceRecordToKafka() throws Exception {
+        when(kafkaTemplate.sendDefault(ArgumentMatchers.anyString(), ArgumentMatchers.any()))
+                .thenReturn(CompletableFuture.completedFuture(null));
+
+        var request = new CreateWorkAttendanceRequest(VALID_CPF, 2024, 10, 180, 180);
 
         mockMvc.perform(post("/work-attendances")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(JsonUtils.asJsonString(request)))
                 .andDo(print())
                 .andExpect(status().isCreated());
+
+        verify(kafkaTemplate, times(1)).sendDefault(ArgumentMatchers.anyString(), ArgumentMatchers.any());
     }
 
-//    @Test
-//    @DisplayName("Should not save null attributes")
-//    void shouldNotSaveNullAttributes() throws Exception {
-//        var request = new CreateWorkAttendanceRequest(null, null, null, null, null);
-//
-//        mockMvc.perform(post("/work-attendances")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(JsonUtils.asJsonString(request)))
-//                .andDo(print())
-//                .andExpect(status().isBadRequest())
-//                .andExpect(jsonPath("$.path", equalTo("/work-attendances")))
-//                .andExpect(jsonPath("$.status", equalTo(HttpStatus.BAD_REQUEST.value())))
-//                .andExpect(jsonPath("$.error", equalTo("Constraint Violation")))
-//                .andExpect(jsonPath("$.message", allOf(
-//                        containsString("employeeCpf"),
-//                        containsString("year"),
-//                        containsString("month"),
-//                        containsString("expectedWorkingHours"),
-//                        containsString("actualWorkingHours")
-//                )));
-//
-//        assertThat(repository.count()).isZero();
-//    }
-//
-//    @Test
-//    @DisplayName("Should not save invalid employeeCpf")
-//    void shouldNotSaveInvalidEmployeeCpf() throws Exception {
-//        var request = new CreateWorkAttendanceRequest(INVALID_CPF, 2024, 10, 180, 180);
-//
-//        mockMvc.perform(post("/work-attendances")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(JsonUtils.asJsonString(request)))
-//                .andDo(print())
-//                .andExpect(status().isBadRequest())
-//                .andExpect(jsonPath("$.path", equalTo("/work-attendances")))
-//                .andExpect(jsonPath("$.status", equalTo(HttpStatus.BAD_REQUEST.value())))
-//                .andExpect(jsonPath("$.error", equalTo("Constraint Violation")))
-//                .andExpect(jsonPath("$.message", containsString("employeeCpf")));
-//
-//        assertThat(repository.count()).isZero();
-//    }
-//
-//    @Test
-//    @DisplayName("Should not save with year less than 1970")
-//    void shouldNotSaveWithYearLessThan1970() throws Exception {
-//        var request = new CreateWorkAttendanceRequest(VALID_CPF, 1000, 10, 180, 180);
-//
-//        mockMvc.perform(post("/work-attendances")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(JsonUtils.asJsonString(request)))
-//                .andDo(print())
-//                .andExpect(status().isBadRequest())
-//                .andExpect(jsonPath("$.path", equalTo("/work-attendances")))
-//                .andExpect(jsonPath("$.status", equalTo(HttpStatus.BAD_REQUEST.value())))
-//                .andExpect(jsonPath("$.error", equalTo("Constraint Violation")))
-//                .andExpect(jsonPath("$.message", containsString("year")));
-//
-//        assertThat(repository.count()).isZero();
-//    }
-//
-//    @Test
-//    @DisplayName("Should not save with month not between 1 and 12")
-//    void shouldNotSaveWithMonthNotBetween1And12() throws Exception {
-//        var request1 = new CreateWorkAttendanceRequest(VALID_CPF, 2000, 0, 180, 180);
-//        var request2 = new CreateWorkAttendanceRequest(VALID_CPF, 2000, 13, 180, 180);
-//
-//        mockMvc.perform(post("/work-attendances")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(JsonUtils.asJsonString(request1)))
-//                .andDo(print())
-//                .andExpect(status().isBadRequest())
-//                .andExpect(jsonPath("$.path", equalTo("/work-attendances")))
-//                .andExpect(jsonPath("$.status", equalTo(HttpStatus.BAD_REQUEST.value())))
-//                .andExpect(jsonPath("$.error", equalTo("Constraint Violation")))
-//                .andExpect(jsonPath("$.message", containsString("month")));
-//
-//        mockMvc.perform(post("/work-attendances")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(JsonUtils.asJsonString(request2)))
-//                .andDo(print())
-//                .andExpect(status().isBadRequest())
-//                .andExpect(jsonPath("$.path", equalTo("/work-attendances")))
-//                .andExpect(jsonPath("$.status", equalTo(HttpStatus.BAD_REQUEST.value())))
-//                .andExpect(jsonPath("$.error", equalTo("Constraint Violation")))
-//                .andExpect(jsonPath("$.message", containsString("month")));
-//
-//        assertThat(repository.count()).isZero();
-//    }
-//
-//    @Test
-//    @DisplayName("Should not save with negative expectedWorkingHours and actualWorkingHours")
-//    void shouldNotSaveWithNegativeExpectedWorkingHoursAndActualWorkingHours() throws Exception {
-//        var request = new CreateWorkAttendanceRequest(VALID_CPF, 1000, 11, -10, -10);
-//
-//        mockMvc.perform(post("/work-attendances")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(JsonUtils.asJsonString(request)))
-//                .andDo(print())
-//                .andExpect(status().isBadRequest())
-//                .andExpect(jsonPath("$.path", equalTo("/work-attendances")))
-//                .andExpect(jsonPath("$.status", equalTo(HttpStatus.BAD_REQUEST.value())))
-//                .andExpect(jsonPath("$.error", equalTo("Constraint Violation")))
-//                .andExpect(jsonPath("$.message", allOf(
-//                        containsString("expectedWorkingHours"),
-//                        containsString("actualWorkingHours"))
-//                ));
-//
-//        assertThat(repository.count()).isZero();
-//    }
-//
-//    @Test
-//    @DisplayName("Should find by employeeCpf, year and month")
-//    void shouldFindByEmployeeCpfYearAndMonth() throws Exception {
-//        int year = 2024;
-//        int month = 10;
-//        var id = new WorkAttendanceRecordId(VALID_CPF, 2024, 10);
-//        var record = repository.saveAndFlush(new WorkAttendanceRecord(id, 180, 180));
-//
-//        mockMvc.perform(get("/work-attendances/{cpf}/{year}/{month}", VALID_CPF, year, month))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.employeeCpf", equalTo(record.getRecordId().getEmployeeCpf())))
-//                .andExpect(jsonPath("$.year", equalTo(record.getRecordId().getYear())))
-//                .andExpect(jsonPath("$.month", equalTo(record.getRecordId().getMonth())))
-//                .andExpect(jsonPath("$.expectedWorkingHours", equalTo(record.getExpectedWorkingHours())))
-//                .andExpect(jsonPath("$.actualWorkingHours", equalTo(record.getActualWorkingHours())));
-//    }
-//
-//    @Test
-//    @DisplayName("Should not find by employeeCpf, year and month and return not found")
-//    void shouldNotFindByEmployeeCpfYearAndMonthAndReturnNotFound() throws Exception {
-//        int year = 2024;
-//        int month = 10;
-//
-//        mockMvc.perform(get("/work-attendances/{cpf}/{year}/{month}", VALID_CPF, year, month))
-//                .andDo(print())
-//                .andExpect(status().isNotFound())
-//                .andExpect(jsonPath("$.path", equalTo(String.format("/work-attendances/%s/%d/%d", VALID_CPF, year, month))))
-//                .andExpect(jsonPath("$.status", equalTo(HttpStatus.NOT_FOUND.value())))
-//                .andExpect(jsonPath("$.error", equalTo("Entity Not Found")))
-//                .andExpect(jsonPath("$.message", containsString("WorkAttendanceRecord")));
-//
-//    }
+    @Test
+    @DisplayName("Should not publish record with null attributes")
+    void shouldNotPublishRecordWithNullAttributes() throws Exception {
+        var request = new CreateWorkAttendanceRequest(null, null, null, null, null);
+
+        mockMvc.perform(post("/work-attendances")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtils.asJsonString(request)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.path", equalTo("/work-attendances")))
+                .andExpect(jsonPath("$.status", equalTo(HttpStatus.BAD_REQUEST.value())))
+                .andExpect(jsonPath("$.error", equalTo("Constraint Violation")))
+                .andExpect(jsonPath("$.message", allOf(
+                        containsString("employeeCpf"),
+                        containsString("year"),
+                        containsString("month"),
+                        containsString("expectedWorkingHours"),
+                        containsString("actualWorkingHours")
+                )));
+
+        verify(kafkaTemplate, never()).sendDefault(ArgumentMatchers.anyString(), ArgumentMatchers.any());
+    }
+
+    @Test
+    @DisplayName("Should not publish record invalid employeeCpf")
+    void shouldNotPublishRecordInvalidEmployeeCpf() throws Exception {
+        var request = new CreateWorkAttendanceRequest(INVALID_CPF, 2024, 10, 180, 180);
+
+        mockMvc.perform(post("/work-attendances")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtils.asJsonString(request)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.path", equalTo("/work-attendances")))
+                .andExpect(jsonPath("$.status", equalTo(HttpStatus.BAD_REQUEST.value())))
+                .andExpect(jsonPath("$.error", equalTo("Constraint Violation")))
+                .andExpect(jsonPath("$.message", containsString("employeeCpf")));
+
+        verify(kafkaTemplate, never()).sendDefault(ArgumentMatchers.anyString(), ArgumentMatchers.any());
+    }
+
+    @Test
+    @DisplayName("Should not publish record with year less than 1970")
+    void shouldNotPublishRecordWithYearLessThan1970() throws Exception {
+        var request = new CreateWorkAttendanceRequest(VALID_CPF, 1000, 10, 180, 180);
+
+        mockMvc.perform(post("/work-attendances")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtils.asJsonString(request)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.path", equalTo("/work-attendances")))
+                .andExpect(jsonPath("$.status", equalTo(HttpStatus.BAD_REQUEST.value())))
+                .andExpect(jsonPath("$.error", equalTo("Constraint Violation")))
+                .andExpect(jsonPath("$.message", containsString("year")));
+
+        verify(kafkaTemplate, never()).sendDefault(ArgumentMatchers.anyString(), ArgumentMatchers.any());
+    }
+
+    @Test
+    @DisplayName("Should not publish record with month not between 1 and 12")
+    void shouldNotPublishRecordWithMonthNotBetween1And12() throws Exception {
+        var request1 = new CreateWorkAttendanceRequest(VALID_CPF, 2000, 0, 180, 180);
+        var request2 = new CreateWorkAttendanceRequest(VALID_CPF, 2000, 13, 180, 180);
+
+        mockMvc.perform(post("/work-attendances")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtils.asJsonString(request1)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.path", equalTo("/work-attendances")))
+                .andExpect(jsonPath("$.status", equalTo(HttpStatus.BAD_REQUEST.value())))
+                .andExpect(jsonPath("$.error", equalTo("Constraint Violation")))
+                .andExpect(jsonPath("$.message", containsString("month")));
+
+        mockMvc.perform(post("/work-attendances")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtils.asJsonString(request2)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.path", equalTo("/work-attendances")))
+                .andExpect(jsonPath("$.status", equalTo(HttpStatus.BAD_REQUEST.value())))
+                .andExpect(jsonPath("$.error", equalTo("Constraint Violation")))
+                .andExpect(jsonPath("$.message", containsString("month")));
+
+        verify(kafkaTemplate, never()).sendDefault(ArgumentMatchers.anyString(), ArgumentMatchers.any());
+    }
+
+    @Test
+    @DisplayName("Should not publish record with negative expectedWorkingHours and actualWorkingHours")
+    void shouldNotPublishRecordWithNegativeExpectedWorkingHoursAndActualWorkingHours() throws Exception {
+        var request = new CreateWorkAttendanceRequest(VALID_CPF, 1000, 11, -10, -10);
+
+        mockMvc.perform(post("/work-attendances")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtils.asJsonString(request)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.path", equalTo("/work-attendances")))
+                .andExpect(jsonPath("$.status", equalTo(HttpStatus.BAD_REQUEST.value())))
+                .andExpect(jsonPath("$.error", equalTo("Constraint Violation")))
+                .andExpect(jsonPath("$.message", allOf(
+                        containsString("expectedWorkingHours"),
+                        containsString("actualWorkingHours"))
+                ));
+
+        verify(kafkaTemplate, never()).sendDefault(ArgumentMatchers.anyString(), ArgumentMatchers.any());
+    }
 }
