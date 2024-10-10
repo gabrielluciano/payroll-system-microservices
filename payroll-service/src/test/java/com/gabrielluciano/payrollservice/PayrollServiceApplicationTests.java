e103b1c5f3ff8393eaba0a92d4af912a949cdb12
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-10-10 10:20:50 -0300

    payroll-service: add testcontainers configuration

--- payroll-service/src/test/java/com/gabrielluciano/payrollservice/PayrollServiceApplicationTests.java
+++ payroll-service/src/test/java/com/gabrielluciano/payrollservice/PayrollServiceApplicationTests.java
@@ -1,8 +1,11 @@
 package com.gabrielluciano.payrollservice;
 
+import com.gabrielluciano.payrollservice.config.TestcontainersConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class PayrollServiceApplicationTests {
 
