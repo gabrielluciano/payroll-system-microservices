3c421ff9fcbabfe47735cb100f347292dc309590
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-10-27 13:35:59 -0300

    api-gateway: expose prometheus metrics

--- api-gateway/src/main/java/com/gabrielluciano/apigateway/SecurityConfig.java
+++ api-gateway/src/main/java/com/gabrielluciano/apigateway/SecurityConfig.java
@@ -1,16 +1,57 @@
 package com.gabrielluciano.apigateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService(
+        @Value("${security.service.username}") String serviceUsername,
+        @Value("${security.service.password}") String servicePassword
+    ) {
+        UserDetails service = User.withUsername(serviceUsername)
+                .password(passwordEncoder().encode(servicePassword))
+                .build();
+        return new MapReactiveUserDetailsService(service);
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityWebFilterChain metricsSecurityWebFilterChain(ServerHttpSecurity http) {
+        http
+            .securityMatcher(ServerWebExchangeMatchers.pathMatchers("/actuator/**"))
+            .authorizeExchange(exchanges -> exchanges
+                .anyExchange().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults())
+            .csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.disable());
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityWebFilterChain oAuth2SecurityWebFilterChain(ServerHttpSecurity http) {
         http
             .authorizeExchange(exchanges -> exchanges
                 .anyExchange().authenticated()
